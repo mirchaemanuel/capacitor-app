bbb9ea74d35beb22c0d8e493f3103ca5161551a5
mirchaemanuel <mirchaemanuel@gmail.com>
2025-01-12 19:49:59 +0100

    rest: experimenting ionic call to backend

--- capacitor-ionic-fe/src/views/Tab1Page.vue
+++ capacitor-ionic-fe/src/views/Tab1Page.vue
@@ -12,12 +12,62 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-button fill="outline" :icon="heart">Default</ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-button size="large">
+              <ion-icon slot="icon-only" :ios="logoApple" :md="settingsSharp"></ion-icon>
+            </ion-button>
+            <ion-button>
+              <ion-icon slot="start" :icon="star"></ion-icon>
+              Left Icon
+            </ion-button>
+            <ion-button color="warning">Default</ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-button id="present-alert">Click Me</ion-button>
+            <ion-alert
+                trigger="present-alert"
+                header="A Short Title Is Best"
+                sub-header="A Sub Header Is Optional"
+                message="A message should be a short, complete sentence."
+                :buttons="alertButtons"
+            ></ion-alert>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-button @click="setOpen()">Click Me</ion-button>
+            <div v-if="isOpen">
+              scemo chi legge
+            </div>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {ref} from 'vue';
+import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonAlert} from '@ionic/vue';
+import {IonButton, IonIcon} from '@ionic/vue';
+import {heart, logoApple, settingsSharp, star} from 'ionicons/icons';
+
+const alertButtons = ['Action', 'ACtion 2'];
+
+const isOpen = ref(false);
+
+const setOpen = () => {
+  isOpen.value = !isOpen.value;
+};
+
 </script>
--- capacitor-ionic-fe/src/views/Tab2Page.vue
+++ capacitor-ionic-fe/src/views/Tab2Page.vue
@@ -11,13 +11,55 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
+      <div id="container">
+        <ion-button @click="callBackend" expand="full">Load Quotes</ion-button>
 
-      <ExploreContainer name="Tab 2 page" />
+        <ion-slides v-if="quotes.length > 0" pager="true">
+          <ion-slide v-for="quote in quotes" :key="quote.id">
+            <ion-card>
+              <ion-card-header>
+                <ion-card-title>{{ quote.author }}</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                {{ quote.quote }}
+              </ion-card-content>
+            </ion-card>
+          </ion-slide>
+        </ion-slides>
+
+        <div v-else class="no-data">
+          <p>No quotes available. Click the button to load them!</p>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+} from '@ionic/vue';
+import axios from 'axios';
+import {ref} from 'vue';
+
+const quotes = ref([]);
+
+const callBackend = async () => {
+  try {
+    const response = await axios.get('http://capacitor-be.test/api/quotes');
+    quotes.value = response.data.data;
+  } catch (error) {
+    console.error('Errore nel recuperare i dati:', error);
+  }
+};
+
 </script>
